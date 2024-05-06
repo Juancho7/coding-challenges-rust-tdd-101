4014dc70f4187915645709820631bb175a9a0f05
Juancho7 <juanfertrom@gmail.com>
2024-05-05 21:03:28 -0500

    feat(medium): resolve exercise 4

---
+++ src/medium/c4_prime_number.rs
@@ -0,0 +1,20 @@
+/*
+ * Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ */
+
+pub fn prime_number() {
+    for i in 2..101 {
+        let mut dividers = 0;
+
+        for j in 2..i {
+            if i % j == 0 {
+                dividers += 1;
+            }
+        }
+
+        if dividers == 0 {
+            println!("{}", i)
+        }
+    }
+}
